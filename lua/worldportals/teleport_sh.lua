37ae335b9b0dfca179543b854d9a8e3b0be17b49
Oninoni <oninoni@oninoni.de>
2019-10-24 00:50:31 +0200

    Fixing Hittrace Weapons

--- lua/worldportals/teleport_sh.lua
+++ lua/worldportals/teleport_sh.lua
@@ -2,14 +2,21 @@
 hook.Add("EntityFireBullets", "WorldPortals_Bullets", function(ent,data)
 	for k,v in pairs(ents.FindByClass("linked_portal_door")) do
 		if not IsValid(v:GetExit()) then continue end
-		local hit,norm,fraction=util.IntersectRayWithOBB(data.Src, data.Dir*16000, v:GetPos(), v:GetAngles(), v:GetCollisionBounds())
-		if hit then
-			v:SetNotSolid(false)
-			local tr=util.QuickTrace(data.Src,data.Dir*16000,{ent})
-			v:SetNotSolid(true)
-			if (tr.Entity==v or v:GetParent()==tr.Entity) and hook.Call("wp-bullet", GAMEMODE, v)~=false then
-				data.Src=wp.TransformPortalPos( tr.HitPos, v, v:GetExit() )
-				data.Dir=wp.TransformPortalVector( data.Dir, v, v:GetExit() )
+
+		local hitPos = util.IntersectRayWithPlane(data.Src, data.Dir, v:GetPos(), v:GetForward())
+
+		if isvector(hitPos) then
+			local localHitPos = v:WorldToLocal(hitPos)
+			local mins, maxs = v:GetCollisionBounds()
+
+			if localHitPos.y > mins.y and localHitPos.y < maxs.y
+			and localHitPos.z > mins.z and localHitPos.z < maxs.z
+			and hook.Call("wp-bullet", GAMEMODE, v)~=false then
+				data.Src=wp.TransformPortalPos( hitPos, v, v:GetExit() )
+							
+				local angle = wp.TransformPortalAngle( data.Dir:Angle(), v, v:GetExit() )
+				data.Dir=angle:Forward()
+				
 				return true
 			end
 		end
