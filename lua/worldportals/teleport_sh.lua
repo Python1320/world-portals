28438926e0cc770033900762f13340f964f0853b
Snailic <joe@megumin.gg>
2021-02-03 15:18:05 -0600

    Fixed incorrect portal selection

--- lua/worldportals/teleport_sh.lua
+++ lua/worldportals/teleport_sh.lua
@@ -1,28 +1,21 @@
 
 hook.Add("EntityFireBullets", "WorldPortals_Bullets", function(ent,data)
-	for k,v in pairs(ents.FindByClass("linked_portal_door")) do
-		if not IsValid(v:GetExit()) then continue end
+	local portal = wp.GetFirstPortalHit(data.Src, data.Dir)
+	if(IsValid(portal.Entity)) then
+		local localHitPos = portal.Entity:WorldToLocal(portal.HitPos)
+		local mins, maxs = portal.Entity:GetCollisionBounds()
+		if localHitPos.y > mins.y and localHitPos.y < maxs.y
+		and localHitPos.z > mins.z and localHitPos.z < maxs.z
+		and hook.Call("wp-trace", GAMEMODE, portal.Entity)~=false then
+			data.Src=wp.TransformPortalPos( portal.HitPos, portal.Entity, portal.Entity:GetExit() )
+			data.Dir=wp.TransformPortalAngle( data.Dir:Angle(), portal.Entity, portal.Entity:GetExit() ):Forward()
 
-		local hitPos = util.IntersectRayWithPlane(data.Src, data.Dir, v:GetPos(), v:GetForward())
-
-		if isvector(hitPos) then
-			local localHitPos = v:WorldToLocal(hitPos)
-			local mins, maxs = v:GetCollisionBounds()
-			if localHitPos.y > mins.y and localHitPos.y < maxs.y
-			and localHitPos.z > mins.z and localHitPos.z < maxs.z
-			and hook.Call("wp-trace", GAMEMODE, v)~=false then
-				data.Src=wp.TransformPortalPos( hitPos, v, v:GetExit() )
-							
-				local angle = wp.TransformPortalAngle( data.Dir:Angle(), v, v:GetExit() )
-				data.Dir=angle:Forward()
-
-				local filter =  hook.Call("wp-tracefilter", GAMEMODE, v)
-				if IsValid(filter) then
-					data.IgnoreEntity = filter
-				end
-				
-				return true
+			local filter =  hook.Call("wp-tracefilter", GAMEMODE, portal.Entity)
+			if IsValid(filter) then
+				data.IgnoreEntity = filter
 			end
+			
+			return true
 		end
 	end
 end)
@@ -30,49 +23,48 @@ end)
 if not util.RealTraceLine then
 	util.RealTraceLine = util.TraceLine
 end
+
 function WorldPortals_TraceLine(data)
 	local trace = util.RealTraceLine(data)
+	local portal = wp.GetFirstPortalHit(trace.StartPos, trace.Normal)
 
-	for k,v in pairs(ents.FindByClass("linked_portal_door")) do
-		if not (v.GetExit and IsValid(v:GetExit())) then continue end
-		local hitPos = util.IntersectRayWithPlane(trace.StartPos, trace.Normal, v:GetPos(), v:GetForward())
+	if(IsValid(portal.Entity)) then
+		local hitPos = portal.HitPos
 
-		if isvector(hitPos) then
-			local localHitPos = v:WorldToLocal(hitPos)
-			local mins, maxs = v:GetCollisionBounds()
-			if localHitPos.y > mins.y and localHitPos.y < maxs.y
-			and localHitPos.z > mins.z and localHitPos.z < maxs.z
-			and trace.Normal:Dot( v:GetForward() ) < 0
-			and hook.Call("wp-trace", GAMEMODE, v)~=false then
-				local angle = wp.TransformPortalAngle( trace.Normal:Angle(), v, v:GetExit() ):Forward()
-				local startPos = wp.TransformPortalPos( hitPos, v, v:GetExit() )
+		local localHitPos = portal.Entity:WorldToLocal(portal.HitPos)
+		local mins, maxs = portal.Entity:GetCollisionBounds()
 
-				local length = data.start:Distance(data.endpos)
-				local usedLength = trace.StartPos:Distance(trace.HitPos)
+		if localHitPos.y > mins.y and localHitPos.y < maxs.y
+		and localHitPos.z > mins.z and localHitPos.z < maxs.z
+		and hook.Call("wp-trace", GAMEMODE, portal.Entity)~=false then
+			local angle = wp.TransformPortalAngle( trace.Normal:Angle(), portal.Entity, portal.Entity:GetExit() ):Forward()
+			local startPos = wp.TransformPortalPos( portal.HitPos, portal.Entity, portal.Entity:GetExit() )
 
-				local endPos = angle
-				endPos:Mul(length + 32 - usedLength)
-				endPos:Add(startPos)
-				
-				local filter = hook.Call("wp-tracefilter", GAMEMODE, v)
-				if IsValid(filter) then
-					if trace.filter then
-						if type(trace.filter) == "table" then
-							table.insert(trace.filter, filter)
-						end
-					else
-						trace.filter = filter
+			local length = data.start:Distance(data.endpos)
+			local usedLength = portal.Distance
+
+			local endPos = angle
+			endPos:Mul(length + 32 - usedLength)
+			endPos:Add(startPos)
+			
+			local filter = hook.Call("wp-tracefilter", GAMEMODE, portal.Entity)
+			if IsValid(filter) then
+				if trace.filter then
+					if type(trace.filter) == "table" then
+						table.insert(trace.filter, filter)
 					end
+				else
+					trace.filter = filter
 				end
-				
-				local tr = util.RealTraceLine({
-					start = startPos,
-					endpos = endPos,
-					mask = trace.mask,
-					filter = trace.filter
-				})
-				return tr
 			end
+			
+			local tr = util.RealTraceLine({
+				start = startPos,
+				endpos = endPos,
+				mask = trace.mask,
+				filter = trace.filter
+			})
+			return tr
 		end
 	end
 	return trace
