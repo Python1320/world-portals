f944b908705952be30a3ee946b33c2614f43cec1
Matt Jeanes <matthew.jeanes@myairops.com>
2021-04-21 17:56:12 +0100

    make sure portal is valid

--- lua/worldportals/render_sv.lua
+++ lua/worldportals/render_sv.lua
@@ -4,7 +4,7 @@ hook.Add( "SetupPlayerVisibility", "WorldPortals_AddPVS", function( ply, ent )
     for _, portal in ipairs( ents.FindByClass( "linked_portal_door" ) ) do
         if ply:TestPVS( portal:GetPos() ) then
             local exitPortal = portal:GetExit()
-            if not ply:TestPVS( exitPortal:GetPos() ) then
+            if IsValid(exitPortal) and (not ply:TestPVS( exitPortal:GetPos() )) then
                 AddOriginToPVS( exitPortal:GetPos() )
             end
         end
